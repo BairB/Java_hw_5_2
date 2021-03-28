b06dcd67af8374346a1da7d1c97da1086ca455ae
Bair B <bairu@yandex.ru>
2021-03-28 19:57:26 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,16 @@
 import java.sql.SQLOutput;
-public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
-    service.calculate(amount, registered);
-//    long expected = 30;
-//    long actual = service.calculate(amount, registered);
-//    boolean passed = expected == actual;
-//    System.out.println(passed);
-  }
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+//        service.calculate(amount, registered);
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+        System.out.println(passed);
+    }
 }
 
